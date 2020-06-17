708c887a978c3a403ec5f284c0d70610504e25c4
Emiliano Suñé <emiliano.sune@gmail.com>
2020-06-16 17:02:06 -0700

    Add vc-authn build and deployment configurations, proof-configurations.

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,3 +1,4 @@
+# Description: The "main" build configuration and vc-authn-oidc deployment profile
 export PROJECT_NAMESPACE="admgjk"
 export GIT_URI="https://github.com/bcgov/devops-credential-issuer.git"
 export GIT_REF="master"
