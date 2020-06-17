d0d92cd21f440f4262b8c55a2a128fbe9e818d84
Emiliano Suñé <emiliano.sune@gmail.com>
2020-06-17 12:17:16 -0700

    Add pipeline configurations

---
+++ src/bc/baseConfig.groovy
@@ -0,0 +1,22 @@
+
+package bc;
+
+class baseConfig {
+  // Wait timeout in minutes
+  public static final int WAIT_TIMEOUT = 10
+
+  // Deployment Environment TAGs
+  public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
+
+  // The name of the project namespace(s).
+  public static final String  NAME_SPACE = 'admgjk'
+
+  // Instance Prefix
+  public static final String  PREFIX = 'vc-authn-'
+
+  // Instance Suffix
+  public static final String  SUFFIX = ''
+
+  // Apps - Listed in the order they should be tagged
+  public static final String[] APPS = ['vc-authn-database', 'vc-authn-agent', 'vc-authn-controller', 'issuer-agent', 'issuer-api', 'issuer-db', 'issuer-wallet', 'issuer-web', 'issuer-admin']
+}
